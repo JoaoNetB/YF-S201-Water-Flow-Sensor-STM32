cb8499239ef0de989b9646e1bef6b17974653be6
João Bevolo <joaobedev@gmail.com~>
2024-12-28 20:10:21 -0300

    liter and milliliter reading functions

--- Include/YF-S201.h
+++ Include/YF-S201.h
@@ -1,4 +1,21 @@
 #ifndef YF_S201_SENSOR_INCLUDE_YF_S201_H_
 #define YF_S201_SENSOR_INCLUDE_YF_S201_H_
 
+#include <stm32c0xx_hal.h>
+
+typedef struct
+{
+	uint16_t GPIO_pin;
+	uint16_t total_pulses;
+	uint16_t pulses_per_liter;
+}YF_S201_HandleTypedef;
+
+void count_pulses(YF_S201_HandleTypedef *YF_S201);
+
+int read_volume_milliliters(YF_S201_HandleTypedef *YF_S201);
+
+int read_volume_liters(YF_S201_HandleTypedef *YF_S201);
+
+
+
 #endif /* YF_S201_SENSOR_INCLUDE_YF_S201_H_ */
--- Source/YF-S201.c
+++ Source/YF-S201.c
@@ -1,1 +1,20 @@
 #include "YF-S201.h"
+
+int read_volume_milliliters(YF_S201_HandleTypedef *YF_S201)
+{
+	int volume = (YF_S201->total_pulses * 1000) / YF_S201->pulses_per_liter;
+
+	return volume;
+}
+
+int read_volume_liters(YF_S201_HandleTypedef *YF_S201)
+{
+	int volume = (YF_S201->total_pulses/YF_S201->pulses_per_liter);
+
+	return volume;
+}
+
+void count_pulses(YF_S201_HandleTypedef *YF_S201)
+{
+	YF_S201->total_pulses++;
+}
