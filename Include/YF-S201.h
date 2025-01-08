ff780ff2a66b7fa5da8f965fe5ed2a93f1c5e2a1
João Bevolo <65148922+JoaoNetB@users.noreply.github.com>
2025-01-08 15:12:37 -0300

    Add comments for functions and sensor structure

--- Include/YF-S201.h
+++ Include/YF-S201.h
@@ -3,17 +3,32 @@
 
 #include <stm32c0xx_hal.h>
 
+/**
+ * @brief Represents the sensor structure. 
+*/
 typedef struct
 {
-	uint16_t GPIO_pin;
-	uint16_t total_pulses;
-	uint16_t pulses_per_liter;
+	uint16_t GPIO_pin; 				// GPIO pin connected to the sensor.
+	uint16_t total_pulses;			// Total number of pulses recorded by the sensor.
+	uint16_t pulses_per_liter;		// Number of pulses equivalent to one liter.
 }YF_S201_HandleTypedef;
 
+/**
+ * @brief Increments the pulse count from the sensor.
+ * @param  YF_S201: Pointer to the sensor handle.
+ */
 void count_pulses(YF_S201_HandleTypedef *YF_S201);
 
+/**
+ * @brief Reads the volume in milliliters.
+ * @param  YF_S201: Pointer to the sensor handle.
+ */
 int read_volume_milliliters(YF_S201_HandleTypedef *YF_S201);
 
+/**
+ * @brief Reads the volume in liters.
+ * @param  YF_S201: Pointer to the sensor handle.
+ */
 int read_volume_liters(YF_S201_HandleTypedef *YF_S201);
 
 
--- Source/YF-S201.c
+++ Source/YF-S201.c
@@ -1,5 +1,10 @@
 #include "YF-S201.h"
 
+/**
+ * @brief Reads the volume in milliliters.
+ * @param  YF_S201: Pointer to the sensor handle
+ * @retval The calculated volume in milliliters.
+ */
 int read_volume_milliliters(YF_S201_HandleTypedef *YF_S201)
 {
 	int volume = (YF_S201->total_pulses * 1000) / YF_S201->pulses_per_liter;
@@ -7,6 +12,11 @@ int read_volume_milliliters(YF_S201_HandleTypedef *YF_S201)
 	return volume;
 }
 
+/**
+ * @brief Reads the volume in liters.
+ * @param  YF_S201: Pointer to the sensor handle
+ * @return The calculated volume in liters.
+ */
 int read_volume_liters(YF_S201_HandleTypedef *YF_S201)
 {
 	int volume = (YF_S201->total_pulses/YF_S201->pulses_per_liter);
@@ -14,6 +24,11 @@ int read_volume_liters(YF_S201_HandleTypedef *YF_S201)
 	return volume;
 }
 
+/**
+ * @brief Increments the pulse count from the sensor.
+ * @param  YF_S201: Pointer to the sensor handle.
+ * @retval None.
+ */
 void count_pulses(YF_S201_HandleTypedef *YF_S201)
 {
 	YF_S201->total_pulses++;
